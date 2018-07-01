325e49c18be65d7dd80061a456858eca34c7a581
lipouli <li.lipou@gmail.com>
2018-07-01 22:03:20 +0200

    ajoutdétail pour entity1 et animation

--- assets/components/Route1/index.js
+++ assets/components/Route1/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import { CSSTransition } from 'react-transition-group';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 import './route1.styl';
 
@@ -10,30 +12,57 @@ import './route1.styl';
 class Route1 extends React.Component {
   state = {
     toc: null,
+    selected: null,
+    data: [],
   };
 
   componentWillMount() {
     axios.get('/data/entity1')
       .then(({ data }) => {
         console.log(data);
-        const toc = data.map((element, index) => (
-          <CSSTransition
-            in
-            timeout={index * 100}
-            classNames="data"
-            unmountOnExit
-            key={element.id}
-            appear
-          >
-            <div className="data">
-              <p>{element.name}</p>
-            </div>
-          </CSSTransition>
-        ));
+        const toc = data.map((element, index) => {
+          const divClass = classNames(
+            'data',
+            {
+              active: this.state.selected === element.id,
+            },
+          );
+          return (
+            <CSSTransition
+              in
+              timeout={index * 100}
+              classNames="data"
+              unmountOnExit
+              key={element.id}
+              appear
+            >
+              <div
+                className={divClass}
+                onClick={this.selectHandler(element.id)}
+                onKeyDown={() => {}}
+              >
+                <p>{element.name}</p>
+              </div>
+            </CSSTransition>
+          );
+        });
+
         this.setState({
           toc,
+          data,
         });
       });
+  }
+
+  selectHandler = selected => () => {
+    this.setState({
+      selected,
+    });
+  }
+  selectRemove = () => {
+    this.setState({
+      selected: null,
+    });
   }
 
   render() {
@@ -43,10 +72,41 @@ class Route1 extends React.Component {
         <div className="data-container">
           {this.state.toc}
         </div>
+        <CSSTransition
+          in={this.state.selected != null}
+          unmountOnExit
+          classNames="info-box"
+          timeout={100}
+        >
+          <InfoBox
+            selectRemove={this.selectRemove}
+            data={this.state.data.find(element => element.id === this.state.selected)}
+          />
+        </CSSTransition>
       </div>
     );
   }
 }
 
+const InfoBox = ({ data, selectRemove }) => (
+  <div className="info-box" onClick={selectRemove} onKeyDown={() => {}}>
+    <div className="info-container">
+      <p>id: {data.id}</p>
+      <p>nom: {data.name}</p>
+      <p>data: {data.data}</p>
+      <p>nom: {data.nom}</p>
+      <p>donnée: {data.donnee}</p>
+    </div>
+  </div>
+);
+
+InfoBox.propTypes = {
+  data: PropTypes.object,
+  selectRemove: PropTypes.func.isRequired,
+};
+
+InfoBox.defaultProps = {
+  data: {},
+};
 
 export default Route1;
